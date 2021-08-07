be128b7fe1cb234175cd7ef7a223b895c9f164af
TheRealRhyle <phillip.j.welch.p@gmail.com>
2021-08-06 23:05:11 -0400

    taking a break for the night. Its slow, but added ~100 words to my auto mod

--- twbba.py
+++ twbba.py
@@ -20,9 +20,9 @@ def goto_twitch(username):
     wordcount = 0
     driver.get(f"https://dashboard.twitch.tv/u/{username}/settings/moderation/blocked-terms")
     logged_in = confirm(text=f"{username}, you will need to manually log into the Chrome window that just opened.", title='Confirm Login', buttons=['Logged in!', 'Get me outta here!'])
-    
+    start_time = time.time()
     if logged_in.lower() == 'logged in!':
-        return add_words(wordcount)
+        return add_words(wordcount), start_time
     else:
         driver.quit()
         sys.exit()
@@ -34,32 +34,37 @@ def add_words(wordcount):
     words = [word.strip('\n') for word in words]
     for word in words:
         search.send_keys(word)
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div/div/div[3]/div[2]/div/div[1]/button"))
-        )
-        element.click()
-
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div/div/div/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div/div/div[3]/div[2]/div/div[2]/div/div/div/div/button[2]/div'))
-        )
-        element.click()
+        time.sleep(1)
         try:
-            element = WebDriverWait(driver, 10).until(
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div/div/div[3]/div[2]/div/div[1]/button"))
+            )
+            element.click()
+            time.sleep(.5)
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div/div/div/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div/div/div[3]/div[2]/div/div[2]/div/div/div/div/button[2]/div'))
+            )
+            element.click()
+            time.sleep(.5)
+            element = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div[2]/button/div/div"))
             )
             element.click()
+            time.sleep(.5)
+            driver.find_element_by_xpath("/html/body/div[1]/div[3]/div/div/div[2]/div[2]/main/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div[1]/div[4]/p")
             search.clear()
+            time.sleep(.5)
+            continue
+        except:
+            search.clear()
+            time.sleep(3)
             wordcount += 1
-        except TimeoutException:
-            search.clear()
             continue
     return wordcount
 
 if __name__ == "__main__":
-    
-    start_time = time.time()
     uname = prompt(text='Twitch username:', title='TWBBA')
-    wordcount = goto_twitch(uname)
+    wordcount, start_time = goto_twitch(uname)
     with open(os.getcwd() + "\\result_log.txt",'w') as lg:
         lg.write(f"{wordcount} words added in {time.time() - start_time} seconds.")
     driver.quit()
